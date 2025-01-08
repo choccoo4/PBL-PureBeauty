3dd74471f7c6422e40d65cd6ba1776626785d4f8
choccoo4 <aulia.anggraini009@gmail.com>
2025-01-08 13:18:26 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,3 +3,10 @@ session_start();
 session_destroy();
 header("Location: login.php");
 exit();
+
+/*
+1. Mulai
+2. Mulai session dan hancurkan session untuk logout
+3. Arahkan pengguna ke halaman login.php
+4. Akhiri
+*/
